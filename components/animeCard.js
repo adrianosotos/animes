0a4bcb564c2be0a4c4bea99275b547d06f61a98e
Adriano <adriano.sotos@linx3.com>
2021-08-26 20:37:40 -0300

    feat: imported right image

--- components/animeCard.js
+++ components/animeCard.js
@@ -3,20 +3,17 @@ import '../styles/animeCard.less'
 
 export default function AnimeCard ({ anime }) {
   return (
-    <>
-      <Image 
-        src={anime.attributes.posterImage.small}
-        alt="anime-cover"
-        loader={() => anime.attributes.coverImage.large}
-        width="284"
-        height="402"
-      />
-      <div>
-        <div className="image-container">
-          
-        </div>
-        <p className="animecard-title">{anime.attributes.titles.en}</p>
+    <div>
+      <div className="image-container">
+        <Image 
+          src={anime.attributes.posterImage.small}
+          alt="anime-cover"
+          loader={() => anime.attributes.posterImage.large}
+          width="284"
+          height="402"
+        />
       </div>
-    </>
+      <p className="animecard-title">{anime.attributes.titles.en}</p>
+    </div>
   )
 }
