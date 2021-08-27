c5e0d017f98451dea317350a28b0328868e7bf83
Adriano <adriano.sotos@linx3.com>
2021-08-26 22:40:29 -0300

    feat: changed anime page layou

--- pages/anime/[id].js
+++ pages/anime/[id].js
@@ -3,26 +3,26 @@ import Image from 'next/image'
 import Video from '../../components/video'
 import '../../styles/animePage.less'
 import { ArrowLeftOutlined } from '@ant-design/icons'
+import { Row, Col } from 'antd';
 import Link from 'next/link'
 
 export default function AnimePage ({ anime }) {
   console.log(anime)
   return (
     <>
+      <Image 
+        src={anime.attributes.coverImage.large}
+        alt="anime-cover"
+        loader={() => anime.attributes.coverImage.large}
+        width="3360"
+        height="800"
+      />
       <div className="container">
         <Link href="/">
           <ArrowLeftOutlined className="back-arrow" />
         </Link>
-        <div className="anime-content">
-          <div className="midia-container">
-            <Video videoId={anime.attributes.youtubeVideoId}/>
-          </div>
-          <div className="content">
-            <h1 className="anime-title">{anime.attributes.titles.en}</h1>
-            <p className="anime-description">{anime.attributes.description}</p>
-          </div>
-        </div>
-        <div className="image-container">
+
+        {/* <div className="image-container">
           <Image 
             src={anime.attributes.posterImage.small}
             alt="anime-cover"
@@ -30,6 +30,21 @@ export default function AnimePage ({ anime }) {
             width="284"
             height="402"
           />
+        </div> */}
+        <div className="anime-content">
+          <Row>
+            <Col xl={12} md={8} xs={24}>
+              <div className="content">
+                <h1 className="anime-title">{anime.attributes.titles.en}</h1>
+                <p className="anime-description">{anime.attributes.description}</p>
+              </div>
+            </Col>
+            <Col xl={12} md={16} xs={24}>
+              <div className="midia-container">
+                <Video videoId={anime.attributes.youtubeVideoId}/>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
     </>
